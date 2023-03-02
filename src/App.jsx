0f2f63c03d9359a6271f662103e42494301349bc
shalash23 <shalashmohamed232@outlook.com>
2023-03-02 14:43:05 +0200

    finished the basics

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { Box, Button, CssBaseline, Typography } from "@mui/material";
+import * as React from "react";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import { themeOptions } from "./utils/theme";
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+import { Container } from "@mui/system";
+import background from "./assets/background4.jpg";
+
+export default function App() {
+  let theme = createTheme(themeOptions);
+  theme = responsiveFontSizes(theme);
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Container
+          disableGutters={true}
+          maxWidth={false}
+          sx={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: "100% 100%",
+            backgroundRepeat: "no-repeat",
+            minHeight: "80vh",
+          }}
+        >
+          <NavBar />
+          <Box
+            sx={{
+              m: "auto",
+              mt: 8,
+
+              width: "min-content",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="h2"
+              component="h1"
+              sx={{
+                fontFamily: "Shantell Sans",
+                color: "#E5E3E4",
+                textTransform: "uppercase",
+                textAlign: "center",
+                mb: 4,
+              }}
+            >
+              Welcome to BookZania
+            </Typography>
+            <Typography
+              variant="body1"
+              component="p"
+              sx={{
+                color: "#E5E3E4",
+                mb: 4,
+              }}
+            >
+              Discover Egypt like never before. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Et, tenetur.
+            </Typography>
+            <Button variant="contained">Contact us</Button>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
---
+++ src/utils/theme.js
@@ -0,0 +1,14 @@
+export const themeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#3A98B9",
+    },
+    secondary: {
+      main: "#FFF1DC",
+    },
+    info: {
+      main: "#E8D5C4",
+    },
+  },
+};
